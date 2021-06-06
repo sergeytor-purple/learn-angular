0844dceea47b39c1d7f774817fc953ac03c75157
serg8master8 <apple500538@gmail.com>
2021-06-06 15:04:26 +0300

    binding

--- part-1/angular-basics/src/app/app.component.ts
+++ part-1/angular-basics/src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-basics';
+  number = 52;
+  arr = [1, 2, 3];
+
+  inputValue = '';
+  constructor() {}
+
+  title = 'initial';
+
+  backgroundToggle = true;
+
+  // onInput(event?: KeyboardEvent) {
+  //   if (event) this.inputValue = (<HTMLInputElement>event.target).value;
+  // }
+
+  onInput(event: any) {
+    this.title = event.target.value;
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
+
+  onClick() {
+    console.log('click');
+  }
 }
--- part-1/angular-basics/src/app/app.module.ts
+++ part-1/angular-basics/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { Post2Component } from './post2/post2.component';
 import { Post3Component } from './post3/post3.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { Post3Component } from './post3/post3.component';
     Post3Component,
     Post4Component,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
