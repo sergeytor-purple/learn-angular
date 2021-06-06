133957c3f437f6af94bfbff33700e73e309e5b98
serg8master8 <apple500538@gmail.com>
2021-06-06 11:22:20 +0300

    create classes and learn cli

---
+++ part-1/angular-basics/src/app/post4/post4.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post4',
+  template: `
+    <div class="post4">
+      <h1>Title</h1>
+    </div>
+  `,
+  styles: [
+    `
+      .post4 {
+        padding: 0.5rem;
+        border: 2px solid black;
+      }
+    `,
+  ],
+})
+export class Post4Component {}
