84be0f1fddba7cbb707cb6ceb243e854d2f28722
serg8master8 <apple500538@gmail.com>
2021-06-07 00:58:43 +0300

    pipes

--- part-1/angular-basics/src/app/app.component.ts
+++ part-1/angular-basics/src/app/app.component.ts
@@ -6,29 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  number = 52;
-  arr = [1, 2, 3];
+  // number = 52;
+  // arr = [1, 2, 3];
+  // inputValue = '';
+  // constructor() {}
+  // title = 'initial';
+  // toggle = true;
 
-  inputValue = '';
-  constructor() {}
-
-  title = 'initial';
-
-  backgroundToggle = true;
-
+  arr = [1, 1, 2, 3, 5, 8, 13];
+  objs = [
+    {
+      title: 'Post1',
+      author: 'Sergey',
+      comments: [
+        { name: 'max', text: 'lorem1' },
+        { name: 'max', text: 'lorem1' },
+        { name: 'max', text: 'lorem1' },
+      ],
+    },
+    {
+      title: 'Post2',
+      author: 'Sergey',
+      comments: [
+        { name: 'max3', text: 'lorem1' },
+        { name: 'max3', text: 'lorem1' },
+        { name: 'max3', text: 'lorem1' },
+      ],
+    },
+  ];
   // onInput(event?: KeyboardEvent) {
   //   if (event) this.inputValue = (<HTMLInputElement>event.target).value;
   // }
-
-  onInput(event: any) {
-    this.title = event.target.value;
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
-  }
-
-  onClick() {
-    console.log('click');
-  }
+  // onInput(event: any) {
+  //   this.title = event.target.value;
+  // }
+  // onBlur(str: string) {
+  //   this.inputValue = str;
+  // }
+  // onClick() {
+  //   console.log('click');
+  // }
 }
