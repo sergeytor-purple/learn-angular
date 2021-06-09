c31a1aac5457b8fdd9ed548416ac742e632e9e2c
serg8master8 <apple500538@gmail.com>
2021-06-10 01:08:24 +0300

    pipes

---
+++ pipes/pipes/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  e: number = Math.E;
+
+  str: string = 'hello world';
+
+  date: Date = new Date()
+}
