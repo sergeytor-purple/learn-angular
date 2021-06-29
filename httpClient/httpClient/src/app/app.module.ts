6d459a03cadaec1dce39a0eac59ff731aff892cf
serg8master8 <apple500538@gmail.com>
2021-06-29 09:03:08 +0300

    learn routing

--- httpClient/httpClient/src/app/app.module.ts
+++ httpClient/httpClient/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptor,
+  multi: true,
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- httpClient/httpClient/src/app/auth.interceptor.ts
+++ httpClient/httpClient/src/app/auth.interceptor.ts
@@ -4,8 +4,10 @@ import {
   HttpEvent,
   HttpHandler,
   HttpRequest,
+  HttpEventType,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -13,6 +15,16 @@ export class AuthInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    const cloned = req.clone({
+      headers: req.headers.append('Auth', 'SOME RANDOM TOKEN'),
+    });
+
+    return next.handle(cloned).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Interceptor Response');
+        }
+      })
+    );
   }
 }
